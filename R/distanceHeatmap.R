66a717c3fada89c274d95639aa91d6af955d17de
AlsammanAlsamman <smahmoud@ageri.sci.eg>
2024-03-15 17:25:34 +0200

    update

--- R/distanceHeatmap.R
+++ R/distanceHeatmap.R
@@ -10,13 +10,13 @@ distanceHeatmap<-function(DistTable,fontsizescale=0)
   # if fontsizescale is not set calculate it
   seqn<-nrow(DistTable)
   if (seqn>1 && seqn<20 && fontsizescale==0) {
-    fontsizescale<-2
-  } else if (seqn>20 && seqn<50&& fontsizescale==0) {
-    fontsizescale<-1
-  } else if (seqn>50 && seqn<100&& fontsizescale==0) {
     fontsizescale<-0.5
-  } else if (seqn>100 && seqn<200&& fontsizescale==0) {
+  } else if (seqn>20 && seqn<50&& fontsizescale==0) {
+    fontsizescale<-0.4
+  } else if (seqn>50 && seqn<100&& fontsizescale==0) {
     fontsizescale<-0.3
+  } else if (seqn>100 && seqn<200&& fontsizescale==0) {
+    fontsizescale<-0.2
   } else if (seqn>200 && fontsizescale==0) {
     fontsizescale<-0.1
   }
--- R/plotSimilarityMatrixWithTree.R
+++ R/plotSimilarityMatrixWithTree.R
@@ -18,12 +18,26 @@ plotSimilarityMatrixWithTree<-function(seqInfo,seqAlignment,
   #HeatMap color Scale
   colors<-colorRampPalette(colors=c("blue","yellow","red"))(20)
   #gene font size
-  genfsize<-100/nrow(DistMatrixTable)
-  if (genfsize>0.8) {
-    genfsize<-0.8
-  }
+  genfsize<-0.8
+  seqnum<-nrow(DistMatrixTable)
+  if (seqnum > 100) {
+    genfsize<- 0.2
+    } else if (seqnum > 80 && seqnum < 100) {
+      genfsize<- 0.3
+    } else if (seqnum > 60 && seqnum < 80) {
+      genfsize<- 0.4
+    } else if (seqnum > 40 && seqnum < 60) {
+      genfsize<- 0.4
+    } else if (seqnum > 20 && seqnum < 40) {
+      genfsize<- 0.5
+    } else if (seqnum < 20) {
+      genfsize<- 0.7
+    }
+
+
+
   figure<-phylo.heatmap(SeqTree,as.matrix(DistMatrixTable),
-                        fsize=c(genfsize,0.8,0.8),
+                        fsize=c(genfsize,genfsize,genfsize),
                         colors=colors,standardize = F)
   figure
 }
